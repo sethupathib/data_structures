f5712f68d3e0da3e43b094398302b7ae6815491c
Sethupathi Balakrishnan <b.sethupathi@gmail.com>
2020-09-16 17:12:59 +0530

    Path Finding (Using Arrays)

---
+++ findPath.cpp
@@ -0,0 +1,71 @@
+//
+//  main.cpp
+//  findPath
+//
+//  Created by Sethupathi on 17/03/20.
+//  Copyright © 2020 Sethupathi. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+
+void findPath(int maze[][20],int n, int x, int y, int path[][20])
+
+{
+    
+    if(x<0 || x> n-1 || y<0 || y> n-1)
+    {
+        return;
+    }
+    
+    if(x==n-1 &&  y==n-1)
+    {
+        path[x][y]=1;
+   
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                cout<<path[i][j]<<" ";
+                
+            }
+            
+            cout<<endl;
+        }
+	cout<<endl;
+        return;
+}   
+
+ 
+    if(maze[x][y]==0 || path[x][y]==1)
+    {
+        return;
+    }
+    
+    path[x][y]=1;
+     findPath(maze, n, x, y+1, path);
+     findPath(maze, n, x, y-1, path);
+     findPath(maze, n, x+1, y, path);
+     findPath(maze, n, x-1, y, path);
+    path[x][y]=0;
+}
+
+void findPath(int maze[][20], int n)
+{
+    
+    int path[20][20] = {0};
+    findPath(maze, n, 0, 0, path);
+}
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+//    std::cout << "Hello, World!\n";
+    
+    int n=3;
+    int maze[20][20] = {{1,1,0},{1,1,0},{0,1,1}};
+    findPath(maze, n);
+    return 0;
+}
+
